0e4862643174e3a27267739862465a7741bcc477
Devin White <WhiteDevint@Gmail.com>
2022-09-02 14:28:26 -0600

    Blending works properly

--- project1.js
+++ project1.js
@@ -6,11 +6,21 @@ function composite( bgImg, fgImg, fgOpac, fgPos )
 {
     for(var i = 0; i < bgImg.data.length; i+=4)
     {
-        bgImg.data[i + 0] =  fgImg.data[i+0] * fgOpac+(1-fgOpac)*bgImg.data[i+0];        // R value
-        bgImg.data[i + 1] =  fgImg.data[i+1] * fgOpac+(1-fgOpac)*bgImg.data[i+1];        // G value
-        bgImg.data[i + 2] =  fgImg.data[i+2] * fgOpac+(1-fgOpac)*bgImg.data[i+2];  // B value
-        bgImg.data[i + 3] = fgOpac + (1-fgOpac) * bgImg.data[i+3];      // A value
+        fgImg.data
+        var alpha = fgOpac + (1-fgOpac) * bgImg.data[i+3];
+           if(alpha == 0 || fgImg.data[i+3] == 0)
+            {
+                bgImg.data = bgImg.data;
+            }
+            else
+            {
+            //bgImg.data[i] =  (fgOpac*fgImg.data[i] +(1-fgOpac)*bgImg.data[i+3]*bgImg.data[i])/alpha;
+            bgImg.data[i] =  (fgOpac*fgImg.data[i] +(1-fgOpac)*bgImg.data[i+3]*bgImg.data[i])/alpha;        // R value
+            bgImg.data[i + 1] =  (fgOpac*fgImg.data[i+1]+(1-fgOpac)*bgImg.data[i+3]*bgImg.data[i+1])/alpha;        // G value
+            bgImg.data[i + 2] =  (fgOpac* fgImg.data[i+2]+(1-fgOpac)*bgImg.data[i+3]*bgImg.data[i+2])/alpha;  // B value
+            }
     }
+    //context.putImageData(fgImg, fgPos.x, fgPos.y);
     //var color = (colorFg*opacityFg+(1-opacityFg)*backgroundColor * alphaBackground)/alpha
     //var alpha = foregroundAlpha + (1-forgroundAlpha)*backgroundAlpha
 
