dd90f960fd48794cb276ca889ab51b4a03133704
Kalean75 <whitedevint@gmail.com>
2022-09-03 18:41:16 -0600

    Blending works properly

--- project1.js
+++ project1.js
@@ -4,21 +4,23 @@
 // fgPos is the position of the foreground image in pixels. It can be negative and (0,0) means the top-left pixels of the foreground and background are aligned.
 function composite( bgImg, fgImg, fgOpac, fgPos )
 {
-    for(var i = 0; i < bgImg.data.length; i+=4)
+    var x = fgPos.x;
+    var y = fgPos.y;
+    for(var i = 0; i < fgImg.data.length; i+=4)
     {
-        fgImg.data
-        var alpha = fgOpac + (1-fgOpac) * bgImg.data[i+3];
-           if(alpha == 0 || fgImg.data[i+3] == 0)
+        var fgRed = fgImg.data[(i - x)];
+        var fgGreen =   fgImg.data[(i-x) + 1];
+        var fgBlue = fgImg.data[(i-x) + 2];
+        var fgAlpha = (fgImg.data[((i-x) + 3)] * fgOpac);
+        var bgAlpha = bgImg.data[i + 3];
+        var alpha = (fgAlpha) + (1.0-fgOpac) * bgAlpha;
+           if(alpha != 0)
             {
-                bgImg.data = bgImg.data;
-            }
-            else
-            {
-            //bgImg.data[i] =  (fgOpac*fgImg.data[i] +(1-fgOpac)*bgImg.data[i+3]*bgImg.data[i])/alpha;
-            bgImg.data[i] =  (fgOpac*fgImg.data[i] +(1-fgOpac)*bgImg.data[i+3]*bgImg.data[i])/alpha;        // R value
-            bgImg.data[i + 1] =  (fgOpac*fgImg.data[i+1]+(1-fgOpac)*bgImg.data[i+3]*bgImg.data[i+1])/alpha;        // G value
-            bgImg.data[i + 2] =  (fgOpac* fgImg.data[i+2]+(1-fgOpac)*bgImg.data[i+3]*bgImg.data[i+2])/alpha;  // B value
-            }
+                bgImg.data[i] =  (fgAlpha * fgRed + (1.0 - fgOpac) * bgAlpha * bgImg.data[i]) / alpha;        // R value
+                bgImg.data[i + 1] =  (fgAlpha * fgGreen + (1.0 - fgOpac) * bgAlpha * bgImg.data[i + 1]) / alpha;        // G value
+                bgImg.data[i + 2] =  (fgAlpha * fgBlue + (1.0 - fgOpac) * bgAlpha * bgImg.data[i + 2]) / alpha; // B value
+                bgImg.data[i + 3] = alpha;  // A value
+            }       
     }
     //context.putImageData(fgImg, fgPos.x, fgPos.y);
     //var color = (colorFg*opacityFg+(1-opacityFg)*backgroundColor * alphaBackground)/alpha
