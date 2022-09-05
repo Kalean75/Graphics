27e374d14aae81a234187aae1e2b8400215f3d6e
Kalean75 <whitedevint@gmail.com>
2022-09-05 00:32:45 -0600

    IT IS DONE!!!

--- project1.js
+++ project1.js
@@ -7,6 +7,7 @@ function composite( bgImg, fgImg, fgOpac, fgPos )
     var offsetX = fgPos.x * 4;
     var offsetY = fgPos.y * bgImg.width * 4;
     var offset = offsetX + offsetY;
+    var maxDrawX = Math.min(fgImg.width, bgImg.width - fgPos.x - 1)*4;
     var fgRed;
     var fgGreen;
     var fgBlue;
@@ -15,24 +16,23 @@ function composite( bgImg, fgImg, fgOpac, fgPos )
     var alpha;
     for(var i = 0; i < fgImg.height*4; i+=4)
     {
-        for(var j = 0; j < fgImg.width*4; j+=4)
+        for(var j = 0; j <maxDrawX; j+=4)
         {
             var bgIndex = i*bgImg.width + j + offset;
             var fgIndex = i*fgImg.width + j;
-            fgRed = fgImg.data[fgIndex];
-            fgGreen =   fgImg.data[fgIndex +1];
-            fgBlue = fgImg.data[fgIndex+ 2];
-            fgAlpha = (fgImg.data[fgIndex+ 3] * fgOpac)/255;
-            bgAlpha = bgImg.data[bgIndex + 3]/255;
-            alpha = (fgAlpha) + (1.0 - fgAlpha) * bgAlpha;
+            fgRed = fgImg.data[fgIndex]; //foreground red
+            fgGreen =   fgImg.data[fgIndex +1]; //foreground green
+            fgBlue = fgImg.data[fgIndex+ 2]; //foreground blue
+            fgAlpha = (fgImg.data[fgIndex+ 3] * fgOpac)/255; //foreground alpha
+            bgAlpha = bgImg.data[bgIndex + 3]/255; //background alpha
+            alpha = fgAlpha + (1.0 - fgAlpha) * bgAlpha; //composite alpha
 
-            if(alpha > 0 && fgAlpha > 0)
-            {
-                bgImg.data[bgIndex] =  (fgAlpha * fgRed + (1.0 - fgAlpha) * bgAlpha * bgImg.data[bgIndex]) / alpha;        // R value
-                bgImg.data[bgIndex+ 1] =  (fgAlpha * fgGreen + (1.0 - fgAlpha) * bgAlpha * bgImg.data[bgIndex +1]) / alpha;        // G value
-                bgImg.data[bgIndex+ 2] =  (fgAlpha * fgBlue + (1.0 - fgAlpha) * bgAlpha * bgImg.data[bgIndex+ 2]) / alpha; // B value
-                //bgImg.data[bgIndex+ 3] = bgAlpha;  // A value
-            }
+            //Adjust RGB of background image
+            //[R,G,B,A]
+            bgImg.data[bgIndex] =  (fgAlpha * fgRed + (1.0 - fgAlpha) * bgAlpha * bgImg.data[bgIndex]) / alpha;        // R value
+            bgImg.data[bgIndex+ 1] =  (fgAlpha * fgGreen + (1.0 - fgAlpha) * bgAlpha * bgImg.data[bgIndex +1]) / alpha;        // G value
+            bgImg.data[bgIndex+ 2] =  (fgAlpha * fgBlue + (1.0 - fgAlpha) * bgAlpha * bgImg.data[bgIndex+ 2]) / alpha; // B value
+            bgImg.data[bgIndex+ 3] = alpha * 255;  // A value
     
         }
     }
