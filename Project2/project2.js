c882abcc56c603bca24c2738b522d83418c21e4c
Kalean75 <whitedevint@gmail.com>
2022-09-07 19:43:39 -0600

    Added scale to matrix

--- Project2/project2.js
+++ Project2/project2.js
@@ -3,7 +3,41 @@
 // The given rotation value is in degrees.
 function GetTransform( positionX, positionY, rotation, scale )
 {
-	return Array( 1, 0, 0, 0, 1, 0, 0, 0, 1 );
+		/*
+	COLUMN MAJOR ORDER
+
+	1 0 0
+	0 1 0
+	0 0 1
+
+	*/
+	//Rotation
+	/*Cos(Theta) -Sin(Theta)
+	Sin(theta)   Cos(Theta)
+	*/
+
+	//translation
+	//P'X = Px + tx
+	//P'Y = Py + ty
+
+	//uniform scale
+	//P' = sP
+	//Uniform scale
+	var identityMatrix = Array( 1, 0, 0, 0, 1, 0, 0, 0, 1 );
+	//uniform scale
+	for(var i = 0; i < identityMatrix.length; i++)
+	{
+		identityMatrix[i] *= scale;
+		alert(identityMatrix[i]);
+	}
+	var pX = positionX * Math.sin(rotation) + positionX * Math.cos(rotation);
+	var pY = positionY * -Math.sin(rotation) + positionY * Math.cos(rotation);
+	for(var i = 0; i< identityMatrix.length; i++)
+	{
+	
+	}
+
+	return identityMatrix;
 }
 
 // Returns a 3x3 transformation matrix as an array of 9 values in column-major order.
