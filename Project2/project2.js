fa2f8d9a5b2a345f431a2a8fa62f06e6cfaaef07
Kalean75 <whitedevint@gmail.com>
2022-09-09 07:32:11 -0600

    Fixed bug with rotation on propellers
    Propeller bug where they would flip when the drone was rotated has been fixed!

--- Project2/project2.js
+++ Project2/project2.js
@@ -4,45 +4,42 @@
 
 function GetTransform(positionX, positionY, rotation, scale) {
 	/*
-COLUMN MAJOR ORDER
+	COLUMN MAJOR ORDER
 
-1 0 0
-0 1 0
-0 0 1
+	------------------------------>   |
+	array[0]	array[3]	array[6]  | array[0]	array[3]	array[6]
+	array[1]	array[4]	array[7]  | array[1]	array[4]	array[7] 
+	array[2]	array[5]	array[8]  v array[2]	array[5]	array[8]
+	{ x1, x2, tx }
+	{ y1, y2, ty }
+	{ 0,  0,  1  }
 
-{ x1, x2, tx }
-{ y1, y2, ty }
-{ 0,  0,  1  }
-
-*/
-	//Rotation
-	/*Cos(Theta) -Sin(Theta)
-	Sin(theta)   Cos(Theta)
 	*/
-/*
-{ cos(rot)*x1, (-sin(rot))*x2, tx }
-{ sin(rot)*y1, cos(rot)*y2,    ty }
-{ 0,           0,              1  }
-*/
+	//Rotation
+	/*
+	{ cos(rot)*x1, (-sin(rot))*y2, tx }
+	{ sin(rot)*x1, cos(rot)*y2,    ty }
+	{ 0,           0,              1  }
+	*/
 	//translation
 	//P'X = Px + tx
 	//P'Y = Py + ty
 
 	//uniform scale
 	//P' = sP
-	//Uniform scale
 	var identityMatrix = Array(1, 0, 0, 0, 1, 0, 0, 0, 1);
 
-	//rotation
-	identityMatrix[0] = Math.cos(rotation*(3.14/180));
-	identityMatrix[1] = Math.sin(rotation*(3.14/180));
-	identityMatrix[4] = Math.cos(rotation*(3.14/180));
-	identityMatrix[3] = -Math.sin(rotation*(3.14/180));
 	//uniform scale
 	for (var i = 0; i < identityMatrix.length; i++) 
 	{
 		identityMatrix[i] *= scale;
 	}
+	//rotation
+	identityMatrix[1] = Math.sin(rotation*(Math.PI/180)) * identityMatrix[0];
+	identityMatrix[3] = -Math.sin(rotation*(Math.PI/180)) * identityMatrix[4];
+	identityMatrix[0] *= Math.cos(rotation*(Math.PI/180));
+	identityMatrix[4] *= Math.cos(rotation*(Math.PI/180));
+
 	//X and y Translation
 	identityMatrix[6] = positionX;
 	identityMatrix[7] = positionY;
@@ -60,32 +57,22 @@ function ApplyTransform(trans1, trans2)
 	array[1]	array[4]	array[7]  | array[1]	array[4]	array[7] 
 	array[2]	array[5]	array[8]  v array[2]	array[5]	array[8]
 	*/
-	var transformationMatrix = Array(1, 0, 0, 0, 1, 0, 0, 0, 1);
-	//first row of a
-	transformationMatrix[0] = (transformationMatrix[0] * trans1[0]) + (transformationMatrix[3] * trans1[1])+(transformationMatrix[6] * trans1[2]);
-	transformationMatrix[3] = (transformationMatrix[0] * trans1[3]) + (transformationMatrix[3] * trans1[4])+(transformationMatrix[6] * trans1[5]);
-	transformationMatrix[6] = (transformationMatrix[0] * trans1[6]) + (transformationMatrix[3] * trans1[7])+(transformationMatrix[6] * trans1[8]);
-	//second row of a
-	transformationMatrix[1] = (transformationMatrix[1] * trans1[0]) + (transformationMatrix[4] * trans1[1])+(transformationMatrix[7] * trans1[2]);
-	transformationMatrix[4] = (transformationMatrix[1] * trans1[3]) + (transformationMatrix[4] * trans1[4])+(transformationMatrix[7] * trans1[5]);
-	transformationMatrix[7] = (transformationMatrix[1] * trans1[6]) + (transformationMatrix[4] * trans1[7])+(transformationMatrix[7] * trans1[8]);
-	//third row of a
-	transformationMatrix[2] = (transformationMatrix[2] * trans1[0]) + (transformationMatrix[5] * trans1[1])+(transformationMatrix[8] * trans1[2]);
-	transformationMatrix[5] = (transformationMatrix[2] * trans1[3]) + (transformationMatrix[5] * trans1[4])+(transformationMatrix[8] * trans1[5]);
-	transformationMatrix[8] = (transformationMatrix[2] * trans1[6]) + (transformationMatrix[5] * trans1[7])+(transformationMatrix[8] * trans1[8]);
 
-	//first row of a
-	transformationMatrix[0] = (transformationMatrix[0] * trans2[0]) + (transformationMatrix[3] * trans2[1])+(transformationMatrix[6] * trans2[2]);
-	transformationMatrix[3] = (transformationMatrix[0] * trans2[3]) + (transformationMatrix[3] * trans2[4])+(transformationMatrix[6] * trans2[5]);
-	transformationMatrix[6] = (transformationMatrix[0] * trans2[6]) + (transformationMatrix[3] * trans2[7])+(transformationMatrix[6] * trans2[8]);
-	//second row of a
-	transformationMatrix[1] = (transformationMatrix[1] * trans2[0]) + (transformationMatrix[4] * trans2[1])+(transformationMatrix[7] * trans2[2]);
-	transformationMatrix[4] = (transformationMatrix[1] * trans2[3]) + (transformationMatrix[4] * trans2[4])+(transformationMatrix[7] * trans2[5]);
-	transformationMatrix[7] = (transformationMatrix[1] * trans2[6]) + (transformationMatrix[4] * trans2[7])+(transformationMatrix[7] * trans2[8]);
-	//third row of a
-	transformationMatrix[2] = (transformationMatrix[2] * trans2[0]) + (transformationMatrix[5] * trans2[1])+(transformationMatrix[8] * trans2[2]);
-	transformationMatrix[5] = (transformationMatrix[2] * trans2[3]) + (transformationMatrix[5] * trans2[4])+(transformationMatrix[8] * trans2[5]);
-	transformationMatrix[8] = (transformationMatrix[2] * trans2[6]) + (transformationMatrix[5] * trans2[7])+(transformationMatrix[8] * trans2[8]);
+	var newMatrix = new Array(9);
 
-	return transformationMatrix;
+	//Apply transformations
+	//first row of a
+	newMatrix[0] = (trans1[0] * trans2[0]) + (trans1[1] * trans2[3])+(trans1[2] * trans2[6]);
+	newMatrix[3] = (trans1[3] * trans2[0]) + (trans1[4] * trans2[3])+(trans1[5] * trans2[6]);
+	newMatrix[6] = (trans1[6] * trans2[0]) + (trans1[7] * trans2[3])+(trans1[8] * trans2[6]);
+	//second row of a
+	newMatrix[1] = (trans1[0] * trans2[1]) + (trans1[1] * trans2[4])+(trans1[2] * trans2[7]);
+	newMatrix[4] = (trans1[3] * trans2[1]) + (trans1[4] * trans2[4])+(trans1[5] * trans2[7]);
+	newMatrix[7] = (trans1[6] * trans2[1]) + (trans1[7] * trans2[4])+(trans1[8] * trans2[7]);
+	//third row of a
+	newMatrix[2] = (trans1[0] * trans2[2]) + (trans1[1] * trans2[5])+(trans1[2] * trans2[8]);
+	newMatrix[5] = (trans1[3] * trans2[2]) + (trans1[4] * trans2[5])+(trans1[5] * trans2[8]);
+	newMatrix[8] = (trans1[6] * trans2[2]) + (trans1[7] * trans2[5])+(trans1[8] * trans2[8]);
+
+	return newMatrix;
 }
