ee6a97c91be0a06c8d7158e31d5bb21a9a1c34de
Kalean75 <whitedevint@gmail.com>
2022-09-26 17:57:31 -0600

    Update

--- Project3/project3.js
+++ Project3/project3.js
@@ -12,12 +12,6 @@ class CurveDrawer
 		// Get the ids of the vertex attributes in the shaders
 		this.vertPos = gl.getAttribLocation(this.prog, 't');
 
-		this.cp = [];
-		for (var i = 0; i < 4; i++) 
-		{
-			this.cp.push(gl.getUniformLocation(this.prog, `p${i}`))
-		}
-
 		// [TO-DO] This is a good place to get the locations of attributes and uniform variables.
 		// Initialize the attribute buffer
 		this.buffer = gl.createBuffer();
@@ -45,13 +39,27 @@ class CurveDrawer
 		// [TO-DO] The control points have changed, we must update corresponding uniform variables.
 		// [TO-DO] Do not forget to bind the program before you set a uniform variable value.
 		// [TO-DO] We can access the x and y coordinates of the i^th control points using
-		// var x = pt[i].getAttribute("cx");
-		// var y = pt[i].getAttribute("cy");
+		//var x = pt[i].getAttribute("cx");
+		//var y = pt[i].getAttribute("cy");
 		gl.useProgram(this.prog);
-		for (var i = 0; i < 4; i++) 
+		this.cp = [];
+		this.cp.push(gl.getUniformLocation(this.prog, `p0`));
+		this.cp.push(gl.getUniformLocation(this.prog, `p1`));
+		this.cp.push(gl.getUniformLocation(this.prog, `p2`));
+		this.cp.push(gl.getUniformLocation(this.prog, `p3` ));
+		/*for (var i = 0; i < 4; i++) 
 		{
-			gl.uniform2fv(this.cp[i], new Float32Array([pt[i].getAttribute("cx"), pt[i].getAttribute("cy")]));
-		}
+			//var x = pt[i].getAttribute("cx");
+			//var y = pt[i].getAttribute("cy");
+			//p.push(x);
+			//p.push(y);
+		}*/
+		gl.uniform2fv(this.cp[0], new Float32Array([pt[0].getAttribute("cx"), pt[0].getAttribute("cy")]));
+		gl.uniform2fv(this.cp[1], new Float32Array([pt[1].getAttribute("cx"), pt[1].getAttribute("cy")]));
+		gl.uniform2fv(this.cp[2], new Float32Array([pt[2].getAttribute("cx"), pt[2].getAttribute("cy")]));
+		gl.uniform2fv(this.cp[3], new Float32Array([pt[3].getAttribute("cx"), pt[3].getAttribute("cy")]));
+		//gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+		//gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(cp), gl.STATIC_DRAW);
 	}
 	draw() {
 		// [TO-DO] This is where we give the command to draw the curve.
@@ -64,7 +72,8 @@ class CurveDrawer
 		gl.drawArrays(gl.LINE_STRIP, 0, this.steps / 2);
 	}
 }
-
+//equation for 4 points
+//P = (1−t)^3*P1 + 3*(1−t)^2tP2 +3(1−t)t^2P3 + t^3P4
 // Vertex Shader
 var curvesVS = `
 	attribute float t;
@@ -75,7 +84,7 @@ var curvesVS = `
 	uniform vec2 p3;
 	void main()
 	{
-		vec2 position = (p0*(1.0-t)*((1.0-t)*(1.0-t)))+(p1*3.0*t*((1.0-t)*(1.0-t)))+(p2*3.0*(1.0-t)*(t*t)+(p3*(t*t)*t));
+		vec2 position = ((1.0-t)*(1.0-t)*(1.0-t)*p0)+(3.0*((1.0-t)*(1.0-t))*t*p1)+(3.0*(1.0-t)*(t*t)*p2)+((t*t*t)*p3);
 		gl_Position = mvp*vec4(position,0,1);
 	}
 `;
