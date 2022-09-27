4c43212801bfbd5f9dbd81d156f0fa953985fe18
Kalean75 <whitedevint@gmail.com>
2022-09-26 18:25:35 -0600

    Finished

--- Project3/project3.js
+++ Project3/project3.js
@@ -1,5 +1,8 @@
-// [TO-DO] Complete the implementation of the following class and the vertex shader below.
 
+//Devin White
+//U0775759
+//CS 4600
+//Project 3 -Bezier Curves
 class CurveDrawer 
 {
 	constructor() 
@@ -12,33 +15,34 @@ class CurveDrawer
 		// Get the ids of the vertex attributes in the shaders
 		this.vertPos = gl.getAttribLocation(this.prog, 't');
 
-		// [TO-DO] This is a good place to get the locations of attributes and uniform variables.
+		// This is a good place to get the locations of attributes and uniform variables.
 		// Initialize the attribute buffer
 		this.buffer = gl.createBuffer();
-		//number of points
+		//num of points?
 		this.steps = 100;
 		var tv = [];
 		for (var i = 0; i < this.steps; ++i) 
 		{
 			tv.push(i / (this.steps - 1));
 		}
-		// [TO-DO] This is where you can create and set the contents of the vertex buffer object
+		// This is where you can create and set the contents of the vertex buffer object
 		// for the vertex attribute we need.
 		gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
 		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(tv), gl.STATIC_DRAW);
 	}
 	setViewport(width, height) {
-		// [TO-DO] This is where we should set the transformation matrix.
-		// [TO-DO] Do not forget to bind the program before you set a uniform variable value.
+		// This is where we should set the transformation matrix.
+		// Do not forget to bind the program before you set a uniform variable value.
 		// Compute the orthographic projection matrix and send it to the shader
 		var trans = [2 / width, 0, 0, 0, 0, -2 / height, 0, 0, 0, 0, 1, 0, -1, 1, 0, 1];
 		gl.useProgram(this.prog);	// Bind the program
 		gl.uniformMatrix4fv(this.mvp, false, trans);
 	}
-	updatePoints(pt) {
-		// [TO-DO] The control points have changed, we must update corresponding uniform variables.
-		// [TO-DO] Do not forget to bind the program before you set a uniform variable value.
-		// [TO-DO] We can access the x and y coordinates of the i^th control points using
+	updatePoints(pt) 
+	{
+		// The control points have changed, we must update corresponding uniform variables.
+		// Do not forget to bind the program before you set a uniform variable value.
+		// We can access the x and y coordinates of the i^th control points using
 		//var x = pt[i].getAttribute("cx");
 		//var y = pt[i].getAttribute("cy");
 		gl.useProgram(this.prog);
@@ -46,24 +50,20 @@ class CurveDrawer
 		this.cp.push(gl.getUniformLocation(this.prog, `p0`));
 		this.cp.push(gl.getUniformLocation(this.prog, `p1`));
 		this.cp.push(gl.getUniformLocation(this.prog, `p2`));
-		this.cp.push(gl.getUniformLocation(this.prog, `p3` ));
-		/*for (var i = 0; i < 4; i++) 
+		this.cp.push(gl.getUniformLocation(this.prog, `p3`));
+		for (var i = 0; i < 4; i++) 
 		{
-			//var x = pt[i].getAttribute("cx");
-			//var y = pt[i].getAttribute("cy");
-			//p.push(x);
-			//p.push(y);
-		}*/
-		gl.uniform2fv(this.cp[0], new Float32Array([pt[0].getAttribute("cx"), pt[0].getAttribute("cy")]));
-		gl.uniform2fv(this.cp[1], new Float32Array([pt[1].getAttribute("cx"), pt[1].getAttribute("cy")]));
-		gl.uniform2fv(this.cp[2], new Float32Array([pt[2].getAttribute("cx"), pt[2].getAttribute("cy")]));
-		gl.uniform2fv(this.cp[3], new Float32Array([pt[3].getAttribute("cx"), pt[3].getAttribute("cy")]));
+			var x = pt[i].getAttribute("cx");
+			var y = pt[i].getAttribute("cy");
+			gl.uniform2fv(this.cp[i], new Float32Array([x, y]));
+		}
 		//gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
 		//gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(cp), gl.STATIC_DRAW);
 	}
-	draw() {
-		// [TO-DO] This is where we give the command to draw the curve.
-		// [TO-DO] Do not forget to bind the program and set the vertex attribute.
+	draw() 
+	{
+		//  This is where we give the command to draw the curve.
+		//  Do not forget to bind the program and set the vertex attribute.
 		// Draw the line segments
 		gl.useProgram(this.prog);
 		gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
