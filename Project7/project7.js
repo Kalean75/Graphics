2c0578de3065be96c63296df70234bff3a47f244
Devin White <WhiteDevint@Gmail.com>
2022-12-05 19:26:50 -0700

    First attempt

--- Project7/project7.js
+++ Project7/project7.js
@@ -2,38 +2,35 @@
 // The two rotations are applied around x and y axes.
 // It returns the combined 4x4 transformation matrix as an array in column-major order.
 // You can use the MatrixMult function defined in project5.html to multiply two 4x4 matrices in the same format.
-function GetModelViewMatrix( translationX, translationY, translationZ, rotationX, rotationY )
-{
-		// [TO-DO] Modify the code below to form the transformation matrix.
-		var rotX = [
-			1, 0, 0, 0,
-			0, Math.cos(rotationX), Math.sin(rotationX), 0,
-			0, -Math.sin(rotationX), Math.cos(rotationX), 0,
-			0, 0, 0, 1
-		];
-		var rotY = [
-			Math.cos(rotationY), 0, -Math.sin(rotationY), 0,
-			0, 1, 0, 0,
-			Math.sin(rotationY), 0, Math.cos(rotationY), 0,
-			0, 0, 0, 1
-		];
-		var trans = [
-			1, 0, 0, 0,
-			0, 1, 0, 0,
-			0, 0, 1, 0,
-			translationX, translationY, translationZ, 1
-		];
-		var rotMatrix = MatrixMult(rotX, rotY);
-		var mv = MatrixMult(trans, rotMatrix);
+function GetModelViewMatrix(translationX, translationY, translationZ, rotationX, rotationY) {
+	// [TO-DO] Modify the code below to form the transformation matrix.
+	var rotX = [
+		1, 0, 0, 0,
+		0, Math.cos(rotationX), Math.sin(rotationX), 0,
+		0, -Math.sin(rotationX), Math.cos(rotationX), 0,
+		0, 0, 0, 1
+	];
+	var rotY = [
+		Math.cos(rotationY), 0, -Math.sin(rotationY), 0,
+		0, 1, 0, 0,
+		Math.sin(rotationY), 0, Math.cos(rotationY), 0,
+		0, 0, 0, 1
+	];
+	var trans = [
+		1, 0, 0, 0,
+		0, 1, 0, 0,
+		0, 0, 1, 0,
+		translationX, translationY, translationZ, 1
+	];
+	var rotMatrix = MatrixMult(rotX, rotY);
+	var mv = MatrixMult(trans, rotMatrix);
 	//var mv = trans;
 	return mv;
-} 
+}
 
-class MeshDrawer
-{
+class MeshDrawer {
 	// The constructor is a good place for taking care of the necessary initializations.
-	constructor()
-	{
+	constructor() {
 		// initializations
 		// Compile the shader program
 		this.swap = false;
@@ -51,7 +48,7 @@ class MeshDrawer
 		//show texture
 		this.showTex = gl.getUniformLocation(this.prog, 'textureShown');
 		//light direction
-		this.lightDir= gl.getUniformLocation(this.prog, 'lightDirection');
+		this.lightDir = gl.getUniformLocation(this.prog, 'lightDirection');
 		//shininess
 		this.shininess = gl.getUniformLocation(this.prog, 'shininess');
 		// Get the ids of the vertex attributes tex coordinates and normals in the shaders
@@ -62,9 +59,9 @@ class MeshDrawer
 		this.vertbuffer = gl.createBuffer();
 		this.texCoordbuffer = gl.createBuffer();
 		this.normbuffer = gl.createBuffer();
-		
+
 	}
-	
+
 	// This method is called every time the user opens an OBJ file.
 	// The arguments of this function is an array of 3D vertex positions,
 	// an array of 2D texture coordinates, and an array of vertex normals.
@@ -76,8 +73,7 @@ class MeshDrawer
 	// form the texture coordinate of a vertex and every three consecutive 
 	// elements in the normals array form a vertex normal.
 	// Note that this method can be called multiple times.
-	setMesh( vertPos, texCoords, normals )
-	{
+	setMesh(vertPos, texCoords, normals) {
 		// Update the contents of the vertex buffer objects.
 		//Vertex
 		gl.bindBuffer(gl.ARRAY_BUFFER, this.vertbuffer);
@@ -90,46 +86,42 @@ class MeshDrawer
 		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
 		this.numTriangles = vertPos.length / 3;
 	}
-	
+
 	// This method is called when the user changes the state of the
 	// "Swap Y-Z Axes" checkbox. 
 	// The argument is a boolean that indicates if the checkbox is checked.
-	swapYZ(swap)
-	{
+	swapYZ(swap) {
 		gl.useProgram(this.prog);
 		// Set the uniform parameter(s) of the vertex shader
 		this.swap = swap;
-		var trans2 = 
-		[
-			1,0,0,0,
-			0,0,1,0,
-			0,1,0,0,
-			0,0,0,1
+		var trans2 =
+			[
+				1, 0, 0, 0,
+				0, 0, 1, 0,
+				0, 1, 0, 0,
+				0, 0, 0, 1
 
-		];
-		if (swap) 
-		{
+			];
+		if (swap) {
 			// Swap Axis
-			gl.uniformMatrix4fv(this.mvp, false, MatrixMult(this.trans,trans2));
+			gl.uniformMatrix4fv(this.mvp, false, MatrixMult(this.trans, trans2));
 			gl.uniformMatrix3fv(this.normalMatrix, false, this.matrixNormal);
 			gl.uniformMatrix4fv(this.mv, false, this.matrixMV);
 		}
-		else
-		{
+		else {
 			//Keep Axis
 			gl.uniformMatrix4fv(this.mvp, false, this.trans);
 			gl.uniformMatrix3fv(this.normalMatrix, false, this.matrixNormal);
 			gl.uniformMatrix4fv(this.mv, false, this.matrixMV);
 		}
 	}
-	
+
 	// This method is called to draw the triangular mesh.
 	// The arguments are the model-view-projection transformation matrixMVP,
 	// the model-view transformation matrixMV, the same matrix returned
 	// by the GetModelViewProjection function above, and the normal
 	// transformation matrix, which is the inverse-transpose of matrixMV.
-	draw( matrixMVP, matrixMV, matrixNormal )
-	{
+	draw(matrixMVP, matrixMV, matrixNormal) {
 		// Complete the WebGL initializations before drawing
 		gl.useProgram(this.prog);
 		//this.normal = MatrixMult(matrixMV,matrixNormal);
@@ -161,13 +153,12 @@ class MeshDrawer
 		gl.drawArrays(gl.TRIANGLES, 0, this.numTriangles);
 	}	// This method is called to set the texture of the mesh.
 	// The argument is an HTML IMG element containing the texture data.
-	setTexture(img)
-	{
+	setTexture(img) {
 		//Bind the texture
 		gl.activeTexture(gl.TEXTURE0);
 		gl.bindTexture(gl.TEXTURE_2D, this.texture);
 		// You can set the texture image data using the following command.
-		this.img = img; 
+		this.img = img;
 		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, img);
 		gl.uniform1i(this.sampler, 1);
 		// Now that we have a texture, it might be a good idea to set
@@ -178,70 +169,62 @@ class MeshDrawer
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
 		//pass in if button is toggled or not when texture initialized
-		if(this.texButton == null)
-		{
+		if (this.texButton == null) {
 			this.showTexture(true);
 		}
-		else
-		{
+		else {
 			this.showTexture(this.texButton);
 		}
 	}
-	
+
 	// This method is called when the user changes the state of the
 	// "Show Texture" checkbox. 
 	// The argument is a boolean that indicates if the checkbox is checked.
-	showTexture( show )
-	{
+	showTexture(show) {
 		// [TO-DO] set the uniform parameter(s) of the fragment shader to specify if it should use the texture.
 		this.texButton = show;
 		// set the uniform parameter(s) of the fragment shader to specify if it should use the texture.
 		gl.useProgram(this.prog);
 		gl.uniform1i(this.showTex, show);
 	}
-	
+
 	// This method is called to set the incoming light direction
-	setLightDir( x, y, z )
-	{
+	setLightDir(x, y, z) {
 		gl.useProgram(this.prog);
 		//set the uniform parameter(s) of the fragment shader to specify the light direction.
-		gl.uniform3fv(this.lightDir, new Float32Array([x,y,z]));
+		gl.uniform3fv(this.lightDir, new Float32Array([x, y, z]));
 	}
-	
+
 	// This method is called to set the shininess of the material
-	setShininess( shininess )
-	{
+	setShininess(shininess) {
 		gl.useProgram(this.prog);
 		// set the uniform parameter(s) of the fragment shader to specify the shininess.
-		gl.uniform1f(this.shininess,shininess);
+		gl.uniform1f(this.shininess, shininess);
 	}
 }
 
 // This function is called for every step of the simulation.
 // Its job is to advance the simulation for the given time step duration dt.
 // It updates the given positions and velocities.
-function SimTimeStep( dt, positions, velocities, springs, stiffness, damping, particleMass, gravity, restitution )
-{
+function SimTimeStep(dt, positions, velocities, springs, stiffness, damping, particleMass, gravity, restitution) {
 	//Fs=k(l-l_rest)*d
 	// l = |x1-x0|
 	// d = (x1-x0)/l
 	//position x
 	//velocity v
 	//initialize f = {0,0,0,....}
-	var forces = Array( positions.length ).fill(new Vec3(0,0,0)); // The total for per particle
+	var forces = Array(positions.length).fill(new Vec3(0, 0, 0)); // The total for per particle
 	//The arrays of positions and velocities contain one 3D vector object of JavaScript class Vec3 per mass particle. 
 	// [TO-DO] Compute the total force of each particle
 	//f = {f0,f1,f2,....,fn-1}
 	//initialize f = {0,0,0,....}
 	//for each particle i
-	for(var i = 0; i < forces.length; i++)
-	{
+	for (var i = 0; i < forces.length; i++) {
 		//add gravity fi = fi+mi+g
-		forces[i] = forces[i].add(gravity).mul(particleMass);
+		forces[i] = gravity.mul(particleMass).add(forces[i]);
 	}
 	//for each spring between particles i and j
-	for(var i = 0; i < springs.length; i++)
-	{
+	for (var i = 0; i < springs.length; i++) {
 		//compute spring forces fs and fd
 		var rest = springs[i].rest;
 		//stretch
@@ -253,28 +236,66 @@ function SimTimeStep( dt, positions, velocities, springs, stiffness, damping, pa
 		//Fs=k(l-l_rest)*d
 		var Fs = d.mul(stiffness * (l - rest));
 		//Fd = kld
-		var Fd = d.mul(damping*ldot);
+		var Fd = d.mul(damping * ldot);
 		//add spring force
 		//fi = fi + (fs+fd)
 		forces[springs[i].p0] = forces[springs[i].p0].add(Fs).add(Fd);
 		//fj = fj - (fs+fd)
-		forces[springs[i].p1] = forces[springs[i].p1].sub(Fs).add(Fd);
+		forces[springs[i].p1] = forces[springs[i].p1].sub(Fs).sub(Fd);
 	}
-	// [TO-DO] Update positions and velocities
-	for(var i = 0; i < positions.length; i++)
+	// Update positions and velocities
+	for (var i = 0; i < positions.length; i++) 
 	{
 		var ai = forces[i].div(particleMass);
-	//update position
-	//xi = xi + deltatvi
-	//update velocity
-	//vi = vi+deltat*ai
-	velocities[i] = ai.mul(dt).add(velocities[i]);
-	positions[i] = velocities[i].mul(dt).add(positions[i]);
+		//update velocity
+		//vi = vi+deltat*ai
+		velocities[i].inc(ai.mul(dt));
+		//update position
+		//xi = xi + deltatvi
+		positions[i].inc(velocities[i].mul(dt));
+		// Handle collisions
+		//h = xz - z0
+		var pos = positions[i];
+		//h' = rh r = resitution coefficient
+		//var bounce = h.mul(restitution);
+		// V'z = -rVz
+		if (pos.y < -1) {
+			var h = pos.y + 1;
+			var bounce = h * restitution;
+			positions[i].y -= h + bounce;
+			velocities[i].y *= -restitution;
+		}
+		else if (pos.y > 1) {
+			var h = pos.y - 1;
+			var bounce = h * restitution;
+			positions[i].y -= h + bounce;
+			velocities[i].y *= -restitution;
+		}
+		else if (pos.x < -1) {
+			var h = pos.x + 1;
+			var bounce = h * restitution;
+			positions[i].x -= h + bounce;
+			velocities[i].x *= -restitution;
+		}
+		else if (pos.x > 1) {
+			var h = pos.x - 1;
+			var bounce = h * restitution;
+			positions[i].x -= h + bounce;
+			velocities[i].x *= -restitution;
+		}
+		else if (pos.z < -1) {
+			var h = pos.z + 1;
+			var bounce = h * restitution;
+			positions[i].z -= h + bounce;
+			velocities[i].z *= -restitution;
+		}
+		else if (pos.z > 1) {
+			var h = pos.z - 1;
+			var bounce = h * restitution;
+			positions[i].z -= h + bounce;
+			velocities[i].z *= -restitution;
+		}
 	}
-	// [TO-DO] Handle collisions
-	//h = xz - z0
-	//h' = rh r = resitution coefficient
-	// V'z = -rVz
 }
 //C = I(n*w) * Kd
 // Vertex shader source code
